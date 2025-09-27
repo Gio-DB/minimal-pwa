8955a0cbb55ad0758e0729b54d7b18e0887dd8c4
Gio-DB <bene.giova@gmail.com>
2025-09-27 08:00:51 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,48 @@
+const CACHE_NAME = 'minimal-pwa-cache-v1';
+const FILES_TO_CACHE = [
+  './',              // Startseite
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install Event: Dateien in Cache legen
 self.addEventListener('install', event => {
-  console.log('📥 Service Worker installiert');
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
   self.skipWaiting();
 });
 
+// Activate Event: alte Caches löschen
 self.addEventListener('activate', event => {
-  console.log('✅ Service Worker aktiviert');
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Entferne alten Cache:', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
   self.clients.claim();
+});
+
+// Fetch Event: aus Cache oder Netzwerk laden
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
